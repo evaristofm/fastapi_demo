1225c165d72382f50faf4352d92079b64c4d3a77
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2023-10-04 11:28:15 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,43 +1,109 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+from fast_zero.database import get_session
+from fast_zero.models import User
+from fast_zero.schemas import Message, Token, UserList, UserPublic, UserSchema
+from fast_zero.security import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    verify_password,
+)
 
 app = FastAPI()
 
-DATABASE = []
+
+@app.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
 
 
 @app.get('/users/', response_model=UserList)
-def read_users():
-    return {'users': DATABASE}
+def read_users(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+    return {'users': users}
 
 
 @app.post(
-    '/users/', status_code=status.HTTP_201_CREATED, response_model=UserPublic
+    '/users/', response_model=UserPublic, status_code=status.HTTP_201_CREATED
 )
-async def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(DATABASE) + 1)
-    DATABASE.append(user_with_id)
-    print(DATABASE)
-    return user_with_id
+async def create_user(
+    user: UserSchema, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(
+        select(User).where(User.username == user.username)
+    )
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Username already registered!',
+        )
+    hashed_password = get_password_hash(user.password)
+    db_user = User(
+        username=user.username, password=hashed_password, email=user.email
+    )
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
 
 
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(DATABASE) or user_id < 1:
-        raise HTTPException(status_code=404, detail='User not found')
+def update_user(
+    user_id: int,
+    user: UserSchema,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Not enough permissions',
+        )
 
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    DATABASE[user_id - 1] = user_with_id
+    current_user = User(**user.model_dump())
+    session.add(current_user)
+    session.commit()
+    session.refresh(current_user)
 
-    return user_with_id
+    return current_user
 
 
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_user(user_id: int):
-    if user_id > len(DATABASE) or user_id < 1:
-        raise HTTPException(status_code=404, detail='User not found')
+def delete_user(
+    user_id: int,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=400, detail='Not enough permissions')
 
-    del DATABASE[user_id - 1]
+    session.delete(current_user)
+    session.commit()
 
     return {'detail': 'User deleted'}
